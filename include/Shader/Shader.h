0a7217d4c07d74250a69759530a7422ef39d5ac1
Letinemeso <sh0n556@bk.ru>
2023-10-04 12:21:35 +0300

    partially removed hard-coded attribute indices

--- include/Shader/Shader.h
+++ include/Shader/Shader.h
@@ -7,6 +7,7 @@
 #include <glew.h>
 #include <mat4x4.hpp>
 
+#include <Data_Structures/Map.h>
 #include <L_Debug/L_Debug.h>
 
 #include <Components/Texture.h>
@@ -17,11 +18,27 @@ namespace LR
 
 	class Shader
     {
+    public:
+        struct Vertex_Attribs
+        {
+            struct Vertex_Data
+            {
+                unsigned int index = 0;
+                unsigned int floats_per_vertex = 0;
+            };
+
+            Vertex_Data coordinates = {0, 0};
+            Vertex_Data colors = {1, 0};
+            Vertex_Data texture_coordinates = {3, 0};
+        };
+
 	private:
         unsigned int m_vertex_shader = 0, m_fragment_shader = 0;
         unsigned int m_program = 0;
 
     private:
+        Vertex_Attribs m_vertex_attribs;
+
         int m_projection_matrix_uniform = -1, m_transform_matrix_uniform = -1, m_texture_uniform = -1;
 
     public:
@@ -37,6 +54,9 @@ namespace LR
         void M_shader_debug(unsigned int _shader) const;
         void M_program_debug(unsigned int _program) const;
 
+    private:
+        void M_verify_vertex_attribs();
+
 	public:
         void init(const std::string& _vertex_shader_source, const std::string& _fragment_shader_source);
         void set_projection_matrix_uniform(const std::string& _name);
@@ -46,6 +66,10 @@ namespace LR
         void set_projection_matrix(const glm::mat4x4& _matrix) const;
         void set_transform_matrix(const glm::mat4x4& _matrix) const;
         void set_texture(const LR::Texture& _texture) const;
+
+    public:
+        inline const Vertex_Attribs& vertex_attribs() const { return m_vertex_attribs; }
+
 	};
 
 }
--- source/Shader/Shader.cpp
+++ source/Shader/Shader.cpp
@@ -89,6 +89,36 @@ void Shader::M_program_debug(unsigned int _program) const
 
 
 
+void Shader::M_verify_vertex_attribs()
+{
+    LDS::Map<std::string, Vertex_Attribs::Vertex_Data*> supported_attributes;       //  TODO: should probably make this configurable
+    supported_attributes.insert("vs_in_pos", &m_vertex_attribs.coordinates);
+    supported_attributes.insert("vs_in_colors", &m_vertex_attribs.colors);
+    supported_attributes.insert("vs_in_tex_coords", &m_vertex_attribs.texture_coordinates);
+
+    int attribs_amount = 0;
+    glGetProgramiv(m_program, GL_ACTIVE_ATTRIBUTES, &attribs_amount);
+
+    const unsigned int variable_name_max_length = 256;
+    char variable_name[variable_name_max_length] = { 0 };
+    int actual_variable_name_length = 0;
+    int variable_size = 0;
+    unsigned int variable_type = 0;
+
+    for(unsigned int i=0; i<(unsigned int)attribs_amount; ++i)
+    {
+        glGetActiveAttrib(m_program, i, variable_name_max_length, &actual_variable_name_length, &variable_size, &variable_type, variable_name);
+
+        LDS::Map<std::string, Vertex_Attribs::Vertex_Data*>::Iterator maybe_attribute_it = supported_attributes.find(std::string(variable_name));
+        L_ASSERT(maybe_attribute_it.is_ok());
+        Vertex_Attribs::Vertex_Data* attribute_data = *maybe_attribute_it;
+
+        attribute_data->floats_per_vertex = 4 - (GL_FLOAT_VEC4 - variable_type);    //  currently GL_FLOAT_VEC3 == GL_FLOAT_VEC2 + 1, GL_FLOAT_VEC4 = GL_FLOAT_VEC3 + 1. hopefully it stays that way
+    }
+}
+
+
+
 void Shader::init(const std::string& _vertex_shader_source, const std::string& _fragment_shader_source)
 {
     glDeleteShader(m_vertex_shader);
@@ -117,6 +147,8 @@ void Shader::init(const std::string& _vertex_shader_source, const std::string& _
     glLinkProgram(m_program);
     L_DEBUG_FUNC_1ARG(M_program_debug, m_program);
     glUseProgram(m_program);
+
+    M_verify_vertex_attribs();
 }
 
 void Shader::set_projection_matrix_uniform(const std::string& _name)
