4bcca91692309d31bd1bc65fee7ef6e9973f2836
Артём <sh0n556@bk.ru>
2025-08-28 13:10:02 +0300

    added sort func for draw layer in Draw_Order_Controller

--- include/Draw_Order_Controller/Draw_Order_Controller.h
+++ include/Draw_Order_Controller/Draw_Order_Controller.h
@@ -15,16 +15,18 @@ namespace LR
     public:
         using Draw_Function = LST::Function<void()>;
         using Before_Draw_Function = LST::Function<void()>;
+        using Sort_Function = LST::Function<bool(const LEti::Object*, const LEti::Object*)>;
 
     private:
-        using Registred_Modules = LDS::Map<LEti::Module*, Draw_Function>;
-        using Registred_Modules_Locations = LDS::Map<LEti::Module*, Registred_Modules*>;
+        using Registered_Modules = LDS::Map<LEti::Module*, Draw_Function>;
+        using Registred_Modules_Locations = LDS::Map<LEti::Module*, Registered_Modules*>;
 
         struct Draw_Layer_Data
         {
             std::string name;
-            Registred_Modules modules_set;
+            Registered_Modules modules_set;
             Before_Draw_Function before_draw;
+            Sort_Function sort_function;
         };
 
         using Draw_Layers = LDS::List<Draw_Layer_Data>;
@@ -40,7 +42,8 @@ namespace LR
             Draw_Layer_Settings(Draw_Layer_Data& _data) : m_data(_data) { }
 
         public:
-            inline void set_before_draw_function(const Before_Draw_Function& _func) { m_data.before_draw = _func; }
+            inline Draw_Layer_Settings& set_before_draw_function(const Before_Draw_Function& _func) { m_data.before_draw = _func; return *this; }
+            inline Draw_Layer_Settings& set_sort_function(const Sort_Function& _func) { m_data.sort_function = _func; return *this; }
 
         };
 
@@ -49,7 +52,7 @@ namespace LR
         Registred_Modules_Locations m_registred_modules_locations;
 
     private:
-        Registred_Modules& M_get_modules_set_for_layer(const std::string& _layer_name);
+        Registered_Modules& M_get_modules_set_for_layer(const std::string& _layer_name);
 
     public:
         Draw_Layer_Settings add_layer(const std::string& _name);
@@ -59,6 +62,10 @@ namespace LR
         void unregister_module(LEti::Module* _module);
 
         bool is_module_registred(LEti::Module* _module);
+
+    private:
+        void M_draw_default(const Registered_Modules& _modules) const;
+        void M_draw_sorted(const Registered_Modules& _modules, const Sort_Function& _func) const;
 
     public:
         void draw() const;
--- source/Draw_Order_Controller/Draw_Order_Controller.cpp
+++ source/Draw_Order_Controller/Draw_Order_Controller.cpp
@@ -1,9 +1,11 @@
 #include <Draw_Order_Controller/Draw_Order_Controller.h>
 
+#include <Stuff/Sorter.h>
+
 using namespace LR;
 
 
-Draw_Order_Controller::Registred_Modules& Draw_Order_Controller::M_get_modules_set_for_layer(const std::string& _layer_name)
+Draw_Order_Controller::Registered_Modules& Draw_Order_Controller::M_get_modules_set_for_layer(const std::string& _layer_name)
 {
     for(Draw_Layers::Iterator it = m_draw_layers.begin(); !it.end_reached(); ++it)
     {
@@ -42,7 +44,7 @@ void Draw_Order_Controller::register_module(const std::string& _layer_name, LEti
     L_ASSERT(_draw_function);
     L_ASSERT(!m_registred_modules_locations.find(_module).is_ok());
 
-    Registred_Modules& modules_in_layer = M_get_modules_set_for_layer(_layer_name);
+    Registered_Modules& modules_in_layer = M_get_modules_set_for_layer(_layer_name);
     modules_in_layer.insert(_module, _draw_function);
 
     m_registred_modules_locations.insert(_module, &modules_in_layer);
@@ -53,9 +55,9 @@ void Draw_Order_Controller::unregister_module(LEti::Module* _module)
     Registred_Modules_Locations::Iterator module_location_it = m_registred_modules_locations.find(_module);
     L_ASSERT(module_location_it.is_ok());
 
-    Registred_Modules& module_location = **module_location_it;
+    Registered_Modules& module_location = **module_location_it;
 
-    Registred_Modules::Iterator module_it = module_location.find(_module);
+    Registered_Modules::Iterator module_it = module_location.find(_module);
     L_ASSERT(module_it.is_ok());
 
     module_location.erase(module_it);
@@ -71,6 +73,39 @@ bool Draw_Order_Controller::is_module_registred(LEti::Module* _module)
 
 
 
+void Draw_Order_Controller::M_draw_default(const Registered_Modules& _modules) const
+{
+    for(Registered_Modules::Const_Iterator module_it = _modules.iterator(); !module_it.end_reached(); ++module_it)
+    {
+        const Draw_Function& draw_function = *module_it;
+        draw_function();
+    }
+}
+
+void Draw_Order_Controller::M_draw_sorted(const Registered_Modules& _modules, const Sort_Function& _func) const
+{
+    L_ASSERT(_func);
+
+    LDS::Vector<Registered_Modules::Const_Iterator> vec_to_sort(_modules.size());
+
+    for(Registered_Modules::Const_Iterator module_it = _modules.iterator(); !module_it.end_reached(); ++module_it)
+        vec_to_sort.push(module_it);
+
+    LST::Sorter<Registered_Modules::Const_Iterator> sorter([&_func](const Registered_Modules::Const_Iterator& _first, const Registered_Modules::Const_Iterator& _second)
+    {
+        return _func(_first.key()->parent_object(), _second.key()->parent_object());
+    });
+    sorter.sort(vec_to_sort);
+
+    for(unsigned int i = 0; i < vec_to_sort.size(); ++i)
+    {
+        const Draw_Function& draw = *(vec_to_sort[i]);
+        draw();
+    }
+}
+
+
+
 void Draw_Order_Controller::draw() const
 {
     for(Draw_Layers::Const_Iterator layer_it = m_draw_layers.begin(); !layer_it.end_reached(); ++layer_it)
@@ -80,11 +115,11 @@ void Draw_Order_Controller::draw() const
         if(layer.before_draw)
             layer.before_draw();
 
-        const Registred_Modules& modules_set = layer.modules_set;
-        for(Registred_Modules::Const_Iterator module_it = modules_set.iterator(); !module_it.end_reached(); ++module_it)
-        {
-            const Draw_Function& draw_function = *module_it;
-            draw_function();
-        }
+        const Registered_Modules& modules_set = layer.modules_set;
+
+        if(layer.sort_function)
+            M_draw_sorted(modules_set, layer.sort_function);
+        else
+            M_draw_default(modules_set);
     }
 }
