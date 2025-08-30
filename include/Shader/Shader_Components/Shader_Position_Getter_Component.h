8ff204e7c0803a8e3b0fa6dddb7bf39d6b6b340f
Артём <sh0n556@bk.ru>
2025-08-30 16:09:35 +0300

    Shader_Position_Getter_Component

---
+++ include/Shader/Shader_Components/Shader_Position_Getter_Component.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <Shader/Shader_Components/Shader_Component.h>
+
+
+namespace LR
+{
+
+    class Shader_Position_Getter_Component : public LR::Shader_Component
+    {
+    public:
+        INIT_VARIABLE(LR::Shader_Position_Getter_Component, LR::Shader_Component)
+
+    private:
+        std::string m_module_position_uniform_name;
+        int m_module_position_uniform = -1;
+
+    public:
+        inline void set_module_position_uniform_name(const std::string& _value) { m_module_position_uniform_name = _value; }
+
+    public:
+        void init(unsigned int _opengl_program_handle) override;
+
+    public:
+        void update(const LR::Draw_Module* _draw_module) override;
+
+    };
+
+
+    class Shader_Position_Getter_Component_Stub : public LR::Shader_Component_Stub
+    {
+    public:
+        INIT_VARIABLE(LR::Shader_Position_Getter_Component_Stub, LR::Shader_Component_Stub)
+
+        INIT_FIELDS
+        ADD_FIELD(std::string, module_position_uniform_name)
+        FIELDS_END
+
+    public:
+        std::string module_position_uniform_name;
+
+    public:
+        INIT_BUILDER_STUB(Shader_Position_Getter_Component)
+
+    };
+
+}
---
+++ source/Shader/Shader_Components/Shader_Position_Getter_Component.cpp
@@ -0,0 +1,37 @@
+#include <Shader/Shader_Components/Shader_Position_Getter_Component.h>
+
+#include <glew.h>
+
+using namespace LR;
+
+
+void Shader_Position_Getter_Component::init(unsigned int _opengl_program_handle)
+{
+    Parent_Type::init(_opengl_program_handle);
+
+    m_module_position_uniform = M_extract_uniform_location(m_module_position_uniform_name);
+}
+
+
+
+void Shader_Position_Getter_Component::update(const LR::Draw_Module* _draw_module)
+{
+    Parent_Type::update(_draw_module);
+
+    const glm::vec3& position = _draw_module->transformation_data()->position();
+    glUniform2f(m_module_position_uniform, position.x, position.y);
+}
+
+
+
+
+
+BUILDER_STUB_DEFAULT_CONSTRUCTION_FUNC(Shader_Position_Getter_Component_Stub)
+
+BUILDER_STUB_INITIALIZATION_FUNC(Shader_Position_Getter_Component_Stub)
+{
+    BUILDER_STUB_PARENT_INITIALIZATION;
+    BUILDER_STUB_CAST_PRODUCT;
+
+    product->set_module_position_uniform_name(module_position_uniform_name);
+}
